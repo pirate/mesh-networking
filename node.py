50f617dac4909aec25e182d54c1da5ca5f07d817
Nick Sweeting <git@nicksweeting.com>
2015-09-26 00:42:30 -0700

    better docstring to explain inq packet queue system

--- node.py
+++ node.py
@@ -12,6 +12,17 @@ from links import VirtualLink, UDPLink, IRCLink
 from programs import Switch, Printer
 from filters import LoopbackFilter, DuplicateFilter, UniqueFilter, StringFilter
 
+# Nodes connect to each other over links.  The node has a runloop that pulls packets off the link's incoming packet Queue, 
+# runs them through its list of filters, then places it in the nodes incoming packet queue for that interface node.inq.
+# the Node's Program is has a seperate runloop in a different thread that is constantly calling node.inq.get().
+# The program does something with the packet (like print it to the screen, or reply with "ACK"), and sends any outgoing responses
+# by calling the Node's send() method directly.  The Node runs the packet through it's outgoing packet filters in order, then
+# if it wasn't dropped, calls the network interface's .send() method to push it over the network.
+
+#  --> incoming packet queue | -> pulls packets off link's inq -> filters -> node.inq |  -> pulls packets off the node's inq
+#              [LINK]        |                         [NODE]                         |               [PROGRAM]
+#  <-- outgoing Link.send()  |   <----  outgoing filters  <-----  Node.send()  <----- |  <- sends responses by calling Node.send()
+
 class Node(threading.Thread):
     """a Node represents a computer.  node.interfaces contains the list of network links the node is connected to.
         Nodes process incoming traffic through their filters, then place packets in their inq for their Program to handle.
@@ -52,6 +63,7 @@ class Node(threading.Thread):
         self.keep_listening = False
         self.program.stop()
         self.join()
+        return True
 
     ### Runloop
 
@@ -98,18 +110,18 @@ if __name__ == "__main__":
     print("[start]-en0                                [end]")
     print("          \[l1]<--vlan2-->[l2]<--irc3:irc5-/\n")
 
-    # ls = (UDPLink('en0', 2014), VirtualLink('vl1'), VirtualLink('vl2'), IRCLink('irc3'), UDPLink('en4', 2016), IRCLink('irc5'))
-    ls = (UDPLink('en0', 2014), VirtualLink('vl1'), VirtualLink('vl2'), VirtualLink('irc3'), UDPLink('en4', 2016), VirtualLink('irc5'))
+    ls = (UDPLink('en0', 2014), VirtualLink('vl1'), VirtualLink('vl2'), IRCLink('irc3'), UDPLink('en4', 2016), IRCLink('irc5'))          # slow, but impressive to connect over IRC
+    # ls = (UDPLink('en0', 2014), VirtualLink('vl1'), VirtualLink('vl2'), VirtualLink('irc3'), UDPLink('en4', 2016), VirtualLink('irc5'))    # faster setup for quick testing
     nodes = (
         Node([ls[0]], 'start'),
         Node([ls[0], ls[2]], 'l1', Program=Switch),
         Node([ls[0], ls[1]], 'r1', Program=Switch),
-        Node([ls[2], ls[3]], 'l2', Filters=[LoopbackFilter, DuplicateFilter], Program=Switch),
-        Node([ls[1], ls[4]], 'r2', Filters=[LoopbackFilter, StringFilter.match(b'red')], Program=Switch),
-        Node([ls[5], ls[4]], 'end'),            # l2 wont forward two of the same packet in a row
-    )                                           # r2 wont forward any packet unless it contains the string 'red'
-    [l.start() for l in ls]
-    [n.start() for n in nodes]
+        Node([ls[2], ls[3]], 'l2', Filters=[LoopbackFilter, DuplicateFilter], Program=Switch),              # l2 wont forward two of the same packet in a row
+        Node([ls[1], ls[4]], 'r2', Filters=[LoopbackFilter, StringFilter.match(b'red')], Program=Switch),   # r2 wont forward any packet unless it contains the string 'red'
+        Node([ls[5], ls[4]], 'end'),
+    )
+    assert all([l.start() for l in ls]),    "Some links failed to start."
+    assert all([n.start() for n in nodes]), "Some nodes failed to start."
     
     try:
         while True:
@@ -119,7 +131,8 @@ if __name__ == "__main__":
             time.sleep(0.5)
 
     except (EOFError, KeyboardInterrupt):
-        [n.stop() for n in nodes]
-        [l.stop() for l in ls]
+        # CTRL-D, CTRL-C
+        print ("Not all", "All")[all([n.stop() for n in nodes])] + " nodes stopped cleanly."
+        print ("Not all", "All")[all([l.stop() for l in ls])]    + " links stopped cleanly."
         print("EXITING")
         exit(0)
