b8586db9fe29fe589039353e2d8b413b5e3ad28c
Nick Sweeting <git@nicksweeting.com>
2015-12-04 15:40:08 -0800

    Update bringitdown.py

--- bringitdown.py
+++ bringitdown.py
@@ -20,6 +20,7 @@ def bring_it_down(iface="en0", spam_packet='HOST:all|GET:spam'):
 
 
 # BEWARE, RUNNING THIS WILL BRING YOUR ENTIRE LOCAL NETWORK TO A HALT, DO NOT RUN IT IF YOU'RE ON A SHARED CONNECTION
-# what this does is write 'HOST:all|GET:spam' directly to your network interface as fast as it can, drowning out legitimate traffic
+# what this does is write 'HOST:all|GET:spam' directly to your network interface as fast as it can, drowning out outer people's legitimate traffic
+# I'm not sure if it's the interference at the physical layer, or if it's the router that gets hammered, either way, Wifi will slow to a halt for everyone connected to the same router.
 if __name__ == "__main__":
     bring_it_down()
