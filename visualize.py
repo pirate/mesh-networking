a45d252dcda46dc0645ceb4a365b446360ddcb4c
Nick Sweeting <git@nicksweeting.com>
2015-12-07 13:01:56 -0800

    rearranged files for bottle server

--- visualize.py
+++ visualize.py
@@ -1,13 +1,25 @@
-from bottle import request, run, route, template, Bottle, abort
+from bottle import request, run, route, template, Bottle, abort, static_file
 from gevent.pywsgi import WSGIServer
 from geventwebsocket import WebSocketError
 from geventwebsocket.handler import WebSocketHandler
 
 app = Bottle()
 
+host, port = ("0.0.0.0", 8080)
+
 @app.route('/')
 def index():
-    return template('static/index.html')
+    return template('static/index.html', host=host, port=port)
+
+@app.get('/static/<path:path>')
+def static_files(path):
+    return static_file(path, root='static/')
+
+
+
+@app.route('/websocket_test')
+def index():
+    return template('static/websocket_test.html', host=host, port=port)
 
 @app.route('/websocket')
 def handle_websocket():
@@ -23,5 +35,6 @@ def handle_websocket():
             break
 
 
-server = WSGIServer(("0.0.0.0", 8080), app, handler_class=WebSocketHandler)
+server = WSGIServer((host, port), app, handler_class=WebSocketHandler)
+print "Starting bottle WSGI + Websocket server %s:%s..." % (host, port)
 server.serve_forever()
