ab00e8588fd0ae0dee6d968db5d12552c87e9c3a
Nick Sweeting <git@nicksweeting.com>
2016-04-05 17:27:38 -0500

    added human-readable protocol parsers for IRC

--- protocols.py
+++ protocols.py
@@ -1,11 +1,13 @@
 from scapy.all import Packet, Ether, TCP, StrField, ShortField, XByteField, IntEnumField
 
+
 class MeshIP(Packet):
     name = 'MeshIP'
     fields_desc = [
         ShortField('source', None),
         ShortField('target', None),
     ]
+
 
 class MeshARP(Packet):
     name = 'MeshARP'
@@ -16,11 +18,22 @@ class MeshARP(Packet):
         ShortField('target', 5),
     ]
 
-class HumanIRC():
-    name = 'HumanIRC'
 
-    def parse(self, message):
-        return message.split(' ', 1)
+class HumanARP(Packet):
+    name = 'HumanARP'
+    fields_desc = [
+        IntEnumField('mode', 1,
+            {1: 'QUERY', 2: 'ANNOUNCE'}
+        ),
+        ShortField('target', 5),
+    ]
+
+class HumanIRC(Packet):
+    name = 'HumanIRC'
+    fields_desc = [
+        StrField('action', ''),
+    ]
+
 
 if __name__ == '__main__':
     test_packet = (Ether() /
