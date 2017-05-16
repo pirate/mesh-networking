eb8e953d22d6231fe6d3ca691290d49cef17e9f9
Nick Sweeting <git@nicksweeting.com>
2017-05-16 10:25:20 -0500

    Update lan_chat.py

--- examples/lan_chat.py
+++ examples/lan_chat.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 # MIT License: Nick Sweeting
 import time
@@ -8,14 +9,14 @@ from mesh.filters import UniqueFilter
 from mesh.node import Node
 
 
-links = [UDPLink('en0', 2010), UDPLink('en1', 2011), UDPLink('en2', 2012), UDPLink('en3', 2013)]
+links = [UDPLink('en0', 2010), UDPLink('en1', 2010), UDPLink('eth0', 2010), UDPLink('eth1', 2010)]  # add your network interface here if it isn't one of these (find the name using ifconfig)
 node = Node(links, 'me', Filters=(UniqueFilter,), Program=Printer)
 [link.start() for link in links]
 node.start()
 
 
 if __name__ == "__main__":
-    print("Run lan-chat.py on another laptop to talk between the two of you on en0.")
+    print("Run lan-chat.py on another computer to talk between yourselves on a LAN.")
     try:
         while True:
             print("------------------------------")
@@ -23,6 +24,6 @@ if __name__ == "__main__":
             node.send(bytes(message, 'UTF-8'))
             time.sleep(0.3)
 
-    except (EOFError, KeyboardInterrupt):   # CTRL-D, CTRL-C
+    except (EOFError, KeyboardInterrupt):   # graceful CTRL-D & CTRL-C
         node.stop()
         [link.stop() for link in links]
